1edb245773053e63c8bedc6606525c56df8c778f
Kristaps <kbebris+git@gmail.com>
2024-01-11 14:25:16 +0200

    working on hw1

--- hw1/c-primer/sizes.c
+++ hw1/c-primer/sizes.c
@@ -4,6 +4,9 @@
 #include <stdlib.h>
 #include <stdint.h>
 
+
+#define PRINT_SIZE(t) (printf("size of %s : %zu bytes \n", #t, sizeof(t)))
+
 int main() {
   // Please print the sizes of the following types:
   // int, short, long, char, float, double, unsigned int, long long
@@ -12,7 +15,7 @@ int main() {
 
   // Here's how to show the size of one type. See if you can define a macro
   // to avoid copy pasting this code.
-  printf("size of %s : %zu bytes \n", "int", sizeof(int));
+  
   // e.g. PRINT_SIZE("int", int);
   //      PRINT_SIZE("short", short);
 
@@ -21,12 +24,31 @@ int main() {
   // you can write
   // e.g. PRINT_SIZE(int);
   //      PRINT_SIZE(short);
+  PRINT_SIZE(int);
+  PRINT_SIZE(short);
+  PRINT_SIZE(long);
+  PRINT_SIZE(char);
+  PRINT_SIZE(float);
+  PRINT_SIZE(double);
+  PRINT_SIZE(unsigned int);
+  PRINT_SIZE(long long);
+  PRINT_SIZE(uint8_t);
+  PRINT_SIZE(uint16_t);
+  PRINT_SIZE(uint32_t);
+  PRINT_SIZE(uint64_t);
+  PRINT_SIZE(uint_fast8_t);
+  PRINT_SIZE(uint_fast16_t);
+  PRINT_SIZE(uintmax_t);
+  PRINT_SIZE(intmax_t);
+  PRINT_SIZE(__int128);
 
   // Composite types have sizes too.
   typedef struct {
     int id;
     int year;
   } student;
+
+  PRINT_SIZE(student);
 
   student you;
   you.id = 12345;
@@ -35,9 +57,14 @@ int main() {
 
   // Array declaration. Use your macro to print the size of this.
   int x[5];
+  PRINT_SIZE(x);
 
   // You can just use your macro here instead: PRINT_SIZE("student", you);
-  printf("size of %s : %zu bytes \n", "student", sizeof(you));
+  PRINT_SIZE(you);
+
+  // White-up 3
+  int a = 7;
+  PRINT_SIZE(&a);
 
   return 0;
 }
